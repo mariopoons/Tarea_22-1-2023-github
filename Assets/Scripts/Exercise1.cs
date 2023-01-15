f93ade5473e15776e68477a7a8a09a3bc008ece3
mariopoons <mariopons02@gmail.com>
2023-01-15 19:08:38 +0100

    ejercicio uno empezado

---
+++ Assets/Scripts/Exercise1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise1 : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public int z;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Arithmetic(int x, int y, int z)
+    {
+
+    }
+
+}
